e74c22ef2452519f17ae2e4ceda060be757d9043
Juan Milano <juan_milano@hotmail.com>
2020-01-28 14:55:49 -0300

    Update strings.go

--- strings.go
+++ strings.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"strings"
 )
-//this comment extra
+//this comment extra...
 func main() {
 	s := "I'm a string"
 	fmt.Printf("Ends with strings? %v\n", strings.HasSuffix(s, "string"))
