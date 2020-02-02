b7708b4b219351cc53490c9667f8297a28f68504
Juan Milano <juan.milano@globant.com>
2020-02-01 23:02:11 -0300

    review date functions

---
+++ handleDate/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	loc, _ := time.LoadLocation("UTC")
+	now := time.Now().In(loc)
+	futureDate := time.Date(2020, 01, 01, 00, 00, 00, 00, time.UTC)
+	fmt.Printf("###############################################################\n")
+
+	diff := futureDate.Sub(now)
+	if futureDate == now {
+		fmt.Println("es la fecha actual", int(diff.Hours()/24))
+	} else {
+		fmt.Println("no es la fecha actual")
+	}
+
+	daystil := int(diff.Hours() / 24)
+
+	if daystil <= 0 {
+		fmt.Println("es la fecha actual %d", int(diff.Hours()/24))
+	} else {
+		fmt.Println("no es la fecha actual dias til", daystil)
+	}
+
+	if now.After(futureDate) {
+		fmt.Println("true")
+	} else {
+		fmt.Println("False")
+	}
+
+}
