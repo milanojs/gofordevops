c0e9dc66d8a07563eeaa1710718846186f866903
Juan Milano <juan.milano@globant.com>
2020-02-02 20:46:30 -0300

    review date

--- handleDate/main.go
+++ handleDate/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	loc, _ := time.LoadLocation("UTC")
 	now := time.Now().In(loc)
-	futureDate := time.Date(2020, 01, 01, 00, 00, 00, 00, time.UTC)
+	futureDate := time.Date(2020, 03, 01, 00, 00, 00, 00, time.UTC)
 	fmt.Printf("###############################################################\n")
 
 	diff := futureDate.Sub(now)
@@ -20,7 +20,7 @@ func main() {
 
 	daystil := int(diff.Hours() / 24)
 
-	if daystil <= 0 {
+	if daystil > 0 {
 		fmt.Println("es la fecha actual %d", int(diff.Hours()/24))
 	} else {
 		fmt.Println("no es la fecha actual dias til", daystil)
