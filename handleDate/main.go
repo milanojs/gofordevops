45b3703e0aa735ae3d63541df9fcb423c07121d3
Juan Milano <juan.milano@globant.com>
2020-02-02 22:24:13 -0300

    review data handling function

--- handleDate/main.go
+++ handleDate/main.go
@@ -9,22 +9,6 @@ func main() {
 	loc, _ := time.LoadLocation("UTC")
 	now := time.Now().In(loc)
 	futureDate := time.Date(2020, 03, 01, 00, 00, 00, 00, time.UTC)
-	fmt.Printf("###############################################################\n")
-
-	diff := futureDate.Sub(now)
-	if futureDate == now {
-		fmt.Println("es la fecha actual", int(diff.Hours()/24))
-	} else {
-		fmt.Println("no es la fecha actual")
-	}
-
-	daystil := int(diff.Hours() / 24)
-
-	if daystil >= 0 {
-		fmt.Printf("es la fecha actual %d\n", int(diff.Hours()/24))
-	} else {
-		fmt.Printf("no es la fecha actual dias til %d\n", daystil)
-	}
 
 	if now.After(futureDate) {
 		fmt.Println("true")
