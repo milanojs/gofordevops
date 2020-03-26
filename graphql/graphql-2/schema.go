8e8cdd82c0ce870b4afdf81a3a59341b33390671
Juan Milano <juan_milano@hotmail.com>
2020-03-25 23:50:27 -0300

    review  graphql 2 with relay

---
+++ graphql/graphql-2/schema.go
@@ -0,0 +1,45 @@
+package data
+
+import (
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/relay"
+)
+
+var postType *graphql.Object
+var queryType *graphql.Object
+var Schema graphql.Schema
+
+func init() {
+	postType = graphql.NewObject(graphql.ObjectConfig{
+		Name: "Post",
+		Fields: graphql.Fields{
+			// Define `id` field as a Relay GlobalID field.
+			// It helps with translating your GraphQL object's id into a global id
+			// For eg:
+			//  For a `Post` type, with an id of `1`, it's global id will be `UG9zdDox`
+			//  which is a base64 encoded version of `Post:1` string
+			// We will explore more in the next part of this series.
+			"id": relay.GlobalIDField("Post", nil),
+			"text": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	})
+
+	queryType = graphql.NewObject(graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			"latestPost": &graphql.Field{
+				Type: postType,
+				Resolve: func(p types.ResolveParams) interface{} {
+					return GetLatestPost()
+				},
+			},
+		},
+	})
+
+	Schema, _ = graphql.NewSchema(graphql.SchemaConfig{
+		Query: queryType,
+	})
+
+}
