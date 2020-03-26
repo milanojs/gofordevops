b559631295ce23fc2c855eb3f5a895383e015f61
Juan Milano <juan_milano@hotmail.com>
2020-03-25 23:35:06 -0300

    add graphql and relay node

---
+++ graphql/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+  "net/http"
+  "github.com/graphql-go/graphql"
+  "github.com/graphql-go/handler"
+)
+
+var queryType = graphql.NewObject(graphql.ObjectConfig{
+    Name: "Query",
+    Fields: graphql.Fields{
+        "latestPost": &graphql.Field{
+            Type: graphql.String,
+            Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "Hello, world!", nil
+			},
+        },
+    },
+})
+
+var Schema, _ = graphql.NewSchema(graphql.SchemaConfig{
+    Query: queryType,
+})
+
+func main() {
+
+  // create a graphl-go HTTP handler with our previously defined schema
+  // and we also set it to return pretty JSON output
+  h := handler.New(&handler.Config{
+    Schema: &Schema,
+    Pretty: true,
+  })
+  
+  // serve a GraphQL endpoint at `/graphql`
+  http.Handle("/graphql", h)
+
+  // and serve!
+  http.ListenAndServe(":8080", nil)
+}
